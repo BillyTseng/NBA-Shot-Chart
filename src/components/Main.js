edc4bc5b450958e2fbb2cd450c7a6c6ed0c89606
Billy Tseng <tseng.billy@gmail.com>
2018-10-15 22:00:26 -0700

    feat: add shots and profile.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import nba from 'nba';
+import { Profile } from "./Profile";
+import { ShotChart } from "./ShotChart";
 
 export class Main extends React.Component {
+    state = {
+        playerInfo: {
+            playerId: 201939
+        }
+    }
+
+    componentDidMount() {
+        const { playerId } = nba.findPlayer('Stephen Curry');
+
+        nba.stats.playerInfo({PlayerID: playerId}).then(
+            (info) => {
+                console.log(info);
+                const playerInfo = {
+                    ...info.commonPlayerInfo[0],
+                    ...info.playerHeadlineStats[0]
+                };
+
+                this.setState({
+                    playerInfo
+                });
+            }
+        );
+    }
+
     render() {
         return (
-          <div className="main">Main page</div>
+          <div className="main">
+            <Profile playerInfo={this.state.playerInfo}/>
+            <ShotChart playerId={this.state.playerInfo.playerId}/>
+          </div>
         );
     }
 }
